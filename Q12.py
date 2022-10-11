da527f3f86bc4617f54d3e5baa04a1a1e737c6d3
bruno anderson <mrbruno60@gmail.com>
2022-10-10 22:30:55 -0300

    questão 12 finalizada

--- Q12.py
+++ Q12.py
@@ -1,3 +1,11 @@
 #Faça um programa que receba uma string, com um número de ponto flutuante, e imprima qual a parte dele que não é inteira;
 
 #inicio
+
+cont = ''
+
+while cont != 's': #loop do progama
+ num = input('bem vindo ao progama de analise,digite a seguir um numero a ser analizado ').split(',')
+ print(num[1])
+ cont = input('analise encerrada,se quiser realizar outra analise,digite quaquer outra tecla,se quiser encerrar digite S ') #condiçaõ para o fim do progama
+print('****progama encerrado****') #fim
